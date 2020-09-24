ae8ddcada2bad2ce9e4b4f335f81311d7d08c196
Antony Tse <4450446+antonysiuming@users.noreply.github.com>
2020-09-25 01:23:31 +0900

    v20200924

---
+++ precache-manifest.18dd23598c7b9a8b0f615bd48b0ebba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb6447b515bdcbf84e14be9533a176b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1417f279d880d93bb418",
+    "url": "./static/css/2.a8f673c4.chunk.css"
+  },
+  {
+    "revision": "6a406005650d3c1d75b4",
+    "url": "./static/css/main.e2d17f3b.chunk.css"
+  },
+  {
+    "revision": "1417f279d880d93bb418",
+    "url": "./static/js/2.2742ef1a.chunk.js"
+  },
+  {
+    "revision": "6a406005650d3c1d75b4",
+    "url": "./static/js/main.f37fc3d3.chunk.js"
+  },
+  {
+    "revision": "69046233bf26ab110678",
+    "url": "./static/js/runtime~main.f536b504.js"
+  }
+]);
